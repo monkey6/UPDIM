938630c70ab5e1dabb81949778be5536466b1c59
monkey <monkeyfanghao@163.com>
2020-12-10 13:35:21 +0800

    程序完成

---
+++ socket_udpclient/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace socket_udpclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxRemoteIP = new System.Windows.Forms.TextBox();
+            this.tbxMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.rtbxMessage = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "发送到：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP：";
+            // 
+            // tbxRemoteIP
+            // 
+            this.tbxRemoteIP.Location = new System.Drawing.Point(106, 12);
+            this.tbxRemoteIP.Name = "tbxRemoteIP";
+            this.tbxRemoteIP.Size = new System.Drawing.Size(249, 21);
+            this.tbxRemoteIP.TabIndex = 2;
+            // 
+            // tbxMessage
+            // 
+            this.tbxMessage.Location = new System.Drawing.Point(13, 257);
+            this.tbxMessage.Multiline = true;
+            this.tbxMessage.Name = "tbxMessage";
+            this.tbxMessage.Size = new System.Drawing.Size(343, 64);
+            this.tbxMessage.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(282, 327);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // rtbxMessage
+            // 
+            this.rtbxMessage.Location = new System.Drawing.Point(12, 53);
+            this.rtbxMessage.Name = "rtbxMessage";
+            this.rtbxMessage.Size = new System.Drawing.Size(343, 195);
+            this.rtbxMessage.TabIndex = 9;
+            this.rtbxMessage.Text = "";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 356);
+            this.Controls.Add(this.rtbxMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbxMessage);
+            this.Controls.Add(this.tbxRemoteIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxRemoteIP;
+        private System.Windows.Forms.TextBox tbxMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox rtbxMessage;
+    }
+}
+
---
+++ socket_udpclient/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+namespace socket_udpclient
+{
+    public partial class Form1 : Form
+    {
+
+        private UdpClient receiveClient;
+        private UdpClient sendClient;
+
+        public struct FLASHWINFO
+        {
+            public UInt32 cbSize;
+            public IntPtr hwnd;
+            public UInt32 dwFlags;
+            public UInt32 uCount;
+            public UInt32 dwTimeout;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        public enum falshType : uint
+        {
+            FLASHW_STOP = 0,    //停止闪烁
+            FALSHW_CAPTION = 1,  //只闪烁标题
+            FLASHW_TRAY = 2,   //只闪烁任务栏
+            FLASHW_ALL = 3,     //标题和任务栏同时闪烁
+            FLASHW_PARAM1 = 4,
+            FLASHW_PARAM2 = 12,
+            FLASHW_TIMER = FLASHW_TRAY | FLASHW_PARAM1,   //无条件闪烁任务栏直到发送停止标志或者窗口被激活，如果未激活，停止时高亮
+            FLASHW_TIMERNOFG = FLASHW_TRAY | FLASHW_PARAM2  //未激活时闪烁任务栏直到发送停止标志或者窗体被激活，停止后高亮
+        }
+
+        public static bool flashTaskBar(IntPtr hWnd, falshType type)
+        {
+            FLASHWINFO fInfo = new FLASHWINFO();
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            fInfo.hwnd = hWnd;//要闪烁的窗口的句柄，该窗口可以是打开的或最小化的
+            fInfo.dwFlags = (uint)type;//闪烁的类型
+            fInfo.uCount = UInt32.MaxValue;//闪烁窗口的次数
+            fInfo.dwTimeout = 0; //窗口闪烁的频度，毫秒为单位；若该值为0，则为默认图标的闪烁频度
+            return FlashWindowEx(ref fInfo);
+        }
+
+        public class Flash
+        {
+            public IntPtr p;
+            public falshType ft;
+            public Flash(IntPtr p, falshType ft)
+            {
+                this.p = p;
+                this.ft = ft;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            IPEndPoint localIpPoint = new IPEndPoint(IPAddress.Any, 51883);
+            receiveClient = new UdpClient(localIpPoint);
+            Thread receiveThread = new Thread(ReceiveMessage);
+            receiveThread.Start(new Flash(this.Handle,falshType.FLASHW_TIMERNOFG));
+        }
+
+        //接受消息的方法
+        public void ReceiveMessage(object obj1)
+        {
+            Flash f = (Flash)obj1;
+            IPEndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+            while (true)
+            {
+                try
+                {
+                    byte[] data = receiveClient.Receive(ref remoteIpPoint);
+                    string message = Encoding.Unicode.GetString(data);
+                    ShowMessageforView(rtbxMessage, string.Format("{0}[{1}]\r\n{2}", remoteIpPoint,DateTime.Now.ToLongTimeString(), message));
+                    flashTaskBar(f.p, f.ft);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+        // 利用委托回调机制实现界面上消息内容显示
+        delegate void ShowMessageforViewCallBack(RichTextBox rtbx, string text);
+        private void ShowMessageforView(RichTextBox rtbx, string text)
+        {
+            if (rtbx.InvokeRequired)
+            {
+                ShowMessageforViewCallBack showMessageforViewCallback = ShowMessageforView;
+                rtbx.Invoke(showMessageforViewCallback, new object[] { rtbx, text });
+            }
+            else
+            {
+                rtbxMessage.AppendText(text);
+                rtbxMessage.AppendText("\r\n\r\n");
+                rtbxMessage.ScrollToCaret();
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(!Regex.IsMatch(tbxRemoteIP.Text, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$"))
+            {
+                MessageBox.Show("请输入正确的IP地址", "提示");
+                return;
+            }
+
+            if (tbxMessage.Text == string.Empty)
+            {
+                MessageBox.Show("发送内容不能为空", "提示");
+                return;
+            }
+            //获取本机的IP
+            string hostName = Dns.GetHostName();   //獲取本機名     
+            IPHostEntry heserver = Dns.GetHostEntry(hostName);
+            foreach (IPAddress curAdd in heserver.AddressList)
+            {
+                if (curAdd.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    sendClient = new UdpClient(new IPEndPoint(curAdd, 0));
+                }
+            }
+            Thread sendThread = new Thread(SendMessage);
+            sendThread.Start(tbxMessage.Text);
+        }
+
+        // 发送消息方法
+        private void SendMessage(object obj)
+        {
+            string message = (string)obj;
+            byte[] sendbytes = Encoding.Unicode.GetBytes(message);
+            IPAddress remoteIp = IPAddress.Parse(tbxRemoteIP.Text);
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(remoteIp, 51883);
+            sendClient.Send(sendbytes, sendbytes.Length, remoteIpEndPoint);
+            sendClient.Close();
+            ShowMessageforView(rtbxMessage, "我["+DateTime.Now.ToLongTimeString()+"]\r\n"+message);
+            // 清空发送消息框
+            ResetMessageText(tbxMessage);
+        }
+
+        // 采用了回调机制
+        // 使用委托实现跨线程界面的操作方式
+        delegate void ResetMessageCallback(TextBox textbox);
+        private void ResetMessageText(TextBox textbox)
+        {
+            // Control.InvokeRequired属性代表
+            // 如果控件的处理与调用线程在不同线程上创建的，则为true,否则为false
+            if (textbox.InvokeRequired)
+            {
+                ResetMessageCallback resetMessagecallback = ResetMessageText;
+                textbox.Invoke(resetMessagecallback, new object[] { textbox });
+            }
+            else
+            {
+                textbox.Clear();
+                textbox.Focus();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            receiveClient.Close();
+        }
+    }
+}
